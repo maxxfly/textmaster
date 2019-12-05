966abf61178ef37912260c5977d6d18b3705366e
JeanMary <jeanmary@gmail.com>
2019-12-05 20:53:11 +0100

    fix tests and create methods

--- rspec/checkout_rspec.rb
+++ rspec/checkout_rspec.rb
@@ -6,7 +6,7 @@ RSpec.describe Checkout do
   let(:fruit_tea_product) { Product.new('FR1', 'Fruit tea', 3.11) }
   let(:apple_product)     { Product.new('AP1', 'Apple', 5.0) }
   let(:coffee_product)    { Product.new('CF1', 'Coffee', 11.23) }
-  
+
   let(:checkout)          { Checkout.new }
 
   before do
@@ -22,19 +22,28 @@ RSpec.describe Checkout do
 
   describe 'with one free product and some other products' do
     it {
-      checkout.scan('FR1')
-      checkout.scan('AP1')
-      checkout.scan('FR1')
-      checkout.scan('CF1')
+      checkout.scan(fruit_tea_product)
+      checkout.scan(apple_product)
+      checkout.scan(fruit_tea_product)
+      checkout.scan(coffee_product)
 
-      expect(checkout.total).to eql 22.25
+      expect(checkout.get_current_quantity(fruit_tea_product)).to eql 2
+      expect(checkout.get_current_quantity(apple_product)).to eql 1
+      expect(checkout.get_current_quantity(coffee_product)).to eql 1
+
+      # in the spec, the expected price is 22.25 but I think that it is an error in the spec
+      expect(checkout.total).to eql 19.34 # 22.25
     }
   end
 
   describe 'with one free product only' do
     it {
-      checkout.scan('FR1')
-      checkout.scan('FR1')
+      checkout.scan(fruit_tea_product)
+      checkout.scan(fruit_tea_product)
+
+      expect(checkout.get_current_quantity(fruit_tea_product)).to eql 2
+      expect(checkout.get_current_quantity(apple_product)).to eql 0
+      expect(checkout.get_current_quantity(coffee_product)).to eql 0
 
       expect(checkout.total).to eql 3.11
     }
@@ -42,10 +51,14 @@ RSpec.describe Checkout do
 
   describe 'with reduc by quantity' do
     it {
-      checkout.scan('AP1')
-      checkout.scan('AP1')
-      checkout.scan('FR1')
-      checkout.scan('AP1')
+      checkout.scan(apple_product)
+      checkout.scan(apple_product)
+      checkout.scan(fruit_tea_product)
+      checkout.scan(apple_product)
+
+      expect(checkout.get_current_quantity(fruit_tea_product)).to eql 1
+      expect(checkout.get_current_quantity(apple_product)).to eql 3
+      expect(checkout.get_current_quantity(coffee_product)).to eql 0
 
       expect(checkout.total).to eql 16.61
     }
